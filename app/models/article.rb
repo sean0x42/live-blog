426045dbec8f2739ee1176aef53fd2cc246e139e
vuvuzella <j.tabacii@gmail.com>
2018-09-28 16:14:20 +1000

    Modified article model, test and fixtures, as per review

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Article Model
 # with validation
 class Article < ApplicationRecord
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,18 +1,15 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-
-  test "Should not save without a title" do
+  test 'Should not save without a title' do
     article = Article.new
     assert_not article.save
   end
 
-  test "Must have minimum of 5 character in title" do
-    article = Article.new title:articles(:fewerthanfive).title
+  test 'Must have minimum of 5 character in title' do
+    article = articles(:fewerthanfive)
     assert_not article.save
   end
-
 end
